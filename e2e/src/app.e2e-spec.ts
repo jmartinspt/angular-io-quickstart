dc258854196f8c79fbef2a688e682532ddad358d
rawlingsj <rawlingsj80@gmail.com>
2018-05-15 20:34:28 +0100

    fix up tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual(' Welcome to angular.io jenkins-x kickstart! ');
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
 });
